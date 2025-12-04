680c8637b78c42ddbccb3b989d649efca68a65d7
sebvam <sebastianmnestor@gmail.com>
2025-12-04 14:31:28 -0300

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,20 @@
+<br>
+<?php 
+	
+	include("libreria/config.php");
+	
+	$con = getChatConnection();
+	$query = "SELECT * FROM ( SELECT * FROM chat order by id DESC LIMIT 3) sub order by id ASC";
+	$run = $con->query($query);
+	while($row = $run->fetch_array()) :
+		?>
+	
+			<div id="chat_data">
+				<span  class="btn btn-default btn-xs"><span class="glyphicon glyphicon-user"></span>
+				<b><?php echo $row['name']; ?></b></span>
+				<span style="float:right;"><?php echo formatDate($row['date']); ?></span>
+				<p style="color:gray;padding-left:20px;"><em><?php echo $row['msg']; ?></em></p>
+			</div>
+			<?php endwhile;?>
+			
+			
