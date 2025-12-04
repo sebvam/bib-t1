5daa52bea9896432186923ef58a2793875436f46
sebvam <sebastianmnestor@gmail.com>
2025-12-04 14:16:03 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start(); //mantiene activa la sesion
-    session_destroy(); //destruye la sesion iniciada
-    header('Location: ./'); //posicionamos la cabecera
-    exit(0); //salida
-?>
